e693fd8d3071c836ca4d58a2148c88bc40c0af2d
Mohammad Kaisar Hamid <mohammadkaisarhamid@gmail.com>
2023-01-11 00:21:00 +0600

    fixed assignment issue

--- src/components/Pages/Review/Review.js
+++ src/components/Pages/Review/Review.js
@@ -6,123 +6,132 @@ import { AuthContext } from "../../../Context/AuthProvider";
 import ReviewRow from "./ReviewRow";
 
 const Review = () => {
-	const { user } = useContext(AuthContext);
-	const [myreviews, setMyreviews] = useState([]);
+  const { user, signOut } = useContext(AuthContext);
+  const [myreviews, setMyreviews] = useState([]);
 
-	useEffect(() => {
-		fetch(`http://localhost:5000/reviews?email=${user?.email}`)
-			.then((res) => res.json())
-			.then((data) => {
-				setMyreviews(data);
-			});
-	}, [user?.email]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/reviews?email=${user?.email}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("dentist-jishan")}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 401 || res.status === 403) {
+          signOut();
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setMyreviews(data);
+      });
+  }, [user?.email]);
 
-	const handleDelete = (id) => {
-		const proceed = Swal.fire({
-			title: "Do you want to delete this review ?",
-			showDenyButton: true,
-			showCancelButton: true,
-			confirmButtonText: "Yes",
-			denyButtonText: "No",
-			customClass: {
-				actions: "my-actions",
-				cancelButton: "order-1 right-gap",
-				confirmButton: "order-2",
-				denyButton: "order-3",
-			},
-		}).then((result) => {
-			if (result.isConfirmed) {
-				Swal.fire("You have successfully deleted this review", "", "success");
-			} else if (result.isDenied) {
-				Swal.fire("Changes are not saved", "", "info");
-			}
-		});
+  const handleDelete = (id) => {
+    const proceed = Swal.fire({
+      title: "Do you want to delete this review ?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+      denyButtonText: "No",
+      customClass: {
+        actions: "my-actions",
+        cancelButton: "order-1 right-gap",
+        confirmButton: "order-2",
+        denyButton: "order-3",
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("You have successfully deleted this review", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
 
-		if (proceed) {
-			fetch(`http://localhost:5000/reviews/${id}`, {
-				method: "DELETE",
-			})
-				.then((res) => res.json())
-				.then((data) => {
-					console.log(data);
-					if (data.deletedCount > 0) {
-						const remaining = myreviews.filter(
-							(indlReview) => indlReview._id !== id
-						);
-						setMyreviews(remaining);
-					}
-				})
-				.catch((err) => console.error(err));
-		}
-	};
+    if (proceed) {
+      fetch(`http://localhost:5000/reviews/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            const remaining = myreviews.filter(
+              (indlReview) => indlReview._id !== id
+            );
+            setMyreviews(remaining);
+          }
+        })
+        .catch((err) => console.error(err));
+    }
+  };
 
-	const handleUpdateStatus = (id) => {
-		fetch(`http://localhost:5000/reviews/${id}`, {
-			method: "PATCH",
-			headers: {
-				"content-type": "application/json",
-			},
-			body: JSON.stringify({ status: "Approved" }),
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data);
-				if (data.modifiedCount > 0) {
-					const remaining = myreviews.filter((myrvw) => myrvw._id !== id);
-					const approved = myreviews.find((myrvw) => myrvw._id === id);
-					const totalReviews = [...remaining, approved];
-					setMyreviews(totalReviews);
-				}
-			});
-	};
-	return (
-		<div className="w-full">
-			<Helmet>
-				<title>Dentist Jishan-Review page</title>
-				<meta name="description" content="This Home Page" />
-				<meta name="keywords" content="Dental, Service, Doctor, Teeth" />
-			</Helmet>
+  const handleUpdateStatus = (id) => {
+    fetch(`http://localhost:5000/reviews/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "Approved" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          const remaining = myreviews.filter((myrvw) => myrvw._id !== id);
+          const approved = myreviews.find((myrvw) => myrvw._id === id);
+          const totalReviews = [...remaining, approved];
+          setMyreviews(totalReviews);
+        }
+      });
+  };
+  return (
+    <div className="w-full">
+      <Helmet>
+        <title>Dentist Jishan-Review page</title>
+        <meta name="description" content="This Home Page" />
+        <meta name="keywords" content="Dental, Service, Doctor, Teeth" />
+      </Helmet>
 
-			<table className="table w-full">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>email</th>
-						<th>Commenting time </th>
-						<th>Comment </th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{myreviews.length === 0 ? (
-						<>
-							<div className="dark:bg-gray-800 dark:text-gray-100">
-								<div className="px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
-									<div className="max-w-3xl mx-auto text-center">
-										<h2 className="text-3xl font-extrabold sm:text-4xl text-center">
-											No reviews yet. Please give a review.
-										</h2>
-									</div>
-								</div>
-							</div>
-						</>
-					) : (
-						<>
-							{myreviews.map((eachReview) => (
-								<ReviewRow
-									key={eachReview._id}
-									handleDelete={handleDelete}
-									eachReview={eachReview}
-									handleUpdateStatus={handleUpdateStatus}
-								></ReviewRow>
-							))}
-						</>
-					)}
-				</tbody>
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>email</th>
+            <th>Commenting time </th>
+            <th>Comment </th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {myreviews.length === 0 ? (
+            <>
+              <div className="dark:bg-gray-800 dark:text-gray-100">
+                <div className="px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                  <div className="max-w-3xl mx-auto text-center">
+                    <h2 className="text-3xl font-extrabold sm:text-4xl text-center">
+                      No reviews yet. Please give a review.
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              {myreviews.map((eachReview) => (
+                <ReviewRow
+                  key={eachReview._id}
+                  handleDelete={handleDelete}
+                  eachReview={eachReview}
+                  handleUpdateStatus={handleUpdateStatus}
+                ></ReviewRow>
+              ))}
+            </>
+          )}
+        </tbody>
 
-				<tfoot></tfoot>
-			</table>
-		</div>
-	);
+        <tfoot></tfoot>
+      </table>
+    </div>
+  );
 };
 export default Review;
